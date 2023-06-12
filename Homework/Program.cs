6938ec2aa907b92a4c74e0ed7be70e431983f4f2
polit <iluhapolitov@gmail.com>
2023-06-12 11:59:34 +0300

    inished 1 task of homework

---
+++ Homework/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+// int [] ArrayCreateAndInput(int l){
+//     int [] array = new int[l];
+//     for(int i = 0;i < l;i++){
+//         Console.Write($"Введите {i + 1} элемент массива: ");
+//         array[i] = Convert.ToInt32(Console.ReadLine());
+//     }
+//     return array;
+// }
+
+// int NumberOfNatural(int [] array){
+
+//     int count = 0;
+
+//     for(int i = 0;i < array.Length;i++){
+//         if(array[i] > 0){
+//             count++;
+//         }
+//     }
+//     return count;
+// }
+
+// Console.Write("Введите количество элементов вводимиого вами массива: ");
+// int l = Convert.ToInt32(Console.ReadLine());
+// int [] mas = ArrayCreateAndInput(l);
+// Console.WriteLine($"{NumberOfNatural(mas)} элементов");
+
+
+// Console.WriteLine($"{count} чисел");
+
+// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+
