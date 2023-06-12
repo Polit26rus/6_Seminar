3b67a05fbd6eda10718ba81fd5686057b27a160e
polit <iluhapolitov@gmail.com>
2023-06-12 12:29:54 +0300

    finished homework

--- Homework/Program.cs
+++ Homework/Program.cs
@@ -34,5 +34,19 @@
 // Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+// Console.Write("k1 = ");
+// int k1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("b1 = ");
+// int b1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("k2 = ");
+// int k2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("b2 = ");
+// int b2 = Convert.ToInt32(Console.ReadLine());
+
+// int x = (k1 - k2)*(b2 - b1);
+// int y = k1 * x + b1;
+
+
+// Console.WriteLine($"({x};{y})");
 
 
